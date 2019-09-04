f1021068f09d32be5a58b73b8b1961138986dbcf
JJ Merelo <jjmerelo@gmail.com>
2019-09-04 20:11:54 +0200

    Cambia nada

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const var = core.getInput('var');
+    core.debug(`Has dicho ${var}`);
   } catch (error) {
     core.setFailed(error.message);
   }
